dc2ab92c64aa3457e52e26badfce3b8c883c9cdf
Maksym <maxkord2003@gmail.com>
2024-05-12 22:18:30 +0300

    Commit 5

--- select_3.py
+++ select_3.py
@@ -1,9 +1,7 @@
-from sqlalchemy import func
-
 from connect_db import session
 from models import Grade
 
-results = session.query(func.round(func.avg(Grade.grade_value), 2).label('avg_grade'))\
+from sqlalchemy import func
 
-for result in results:
-    print(result.avg_grade)
+average_grade = session.query(func.round(func.avg(Grade.grade_value), 2)).scalar()
+print(average_grade)
